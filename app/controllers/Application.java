73f0cdd14ce76c36e65b32ac609566010956f60d
Natan Ribeiro <natan.ribeiro@ccc.ufcg.eud.br>
2016-07-18 13:19:29 -0300

    Update Application.java

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -5,8 +5,19 @@ import play.mvc.Result;
 
 public class Application extends Controller {
     
+    private List<User> listUsers = new ArrayList<>();
+
     public static Result index() {
         return ok(views.html.index.render("Green Box"));
     }
+
+    public static Result cadastro() {
+        return ok(cadastro.render());
+    }
+
+    public static  Result login() {
+        return ok(home.render());
+    }
+
     
 }
